73eb702a421e57565e9dbb33f0e261a89f7a1400
Salaxiddinov Suxrob <salaxiddinovsuxrob@gamil.com>
2020-08-08 17:42:32 +0400

    Edit

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://deploy.ru'
+    ? 'https://react-reserve-git-master.insidesteep.vercel.app'
     : 'http://localhost:3000'
 
 export default baseUrl
